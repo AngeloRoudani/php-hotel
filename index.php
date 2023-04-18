482a4a60bc2ce4b88b7aaa1e3f713daa594a9591
AngeloRoudani <ange.rouda@gmail.com>
2023-04-18 17:04:44 +0200

    stampa  degli hotel

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Hotels</title>
 </head>
 <body>
@@ -48,13 +49,59 @@
         ],
 
     ];
-    
+
+    $title = '';
+    $description = '';
+
     foreach($hotels as $hotel) {
+        
         foreach($hotel as $key => $data) {
-            echo "<div>". $data . "</div>";
+            echo  $key . $data . '<br>';
+
         }
-    }
+    };
 
 ?>
+
+    <header>
+        <h1 class=text-center >Boo-king</h1>
+    </header>
+
+    <main>
+            <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col"><?php $hotels[0]['name'] ?></th>
+            <th scope="col"><?php $hotels[1]['name'] ?></th>
+            <th scope="col"><?php $hotels[2]['name'] ?></th>
+            <th scope="col"><?php $hotels[3]['name'] ?></th>
+            <th scope="col"><?php $hotels[4]['name'] ?></th>
+            
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td></td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+        
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
